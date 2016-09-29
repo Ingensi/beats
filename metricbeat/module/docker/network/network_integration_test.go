2945c1437b846caac981cc9d67f0b8e40da6c938
Douae Jeouit <douae.jeouit@gmail.com>
2016-09-29 16:38:40 +0200

    add integration tests to network metricset

--- metricbeat/module/docker/network/network_integration_test.go
+++ metricbeat/module/docker/network/network_integration_test.go
@@ -1,5 +1,8 @@
+<<<<<<< b15c311f2fa9a3beb220fcf17cddcff370c7abcc
 // +build integration
 
+=======
+>>>>>>> add integration tests to network metricset
 package network
 
 import (
@@ -8,12 +11,22 @@ import (
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
 
+<<<<<<< b15c311f2fa9a3beb220fcf17cddcff370c7abcc
 func TestData(t *testing.T) {
 	f := mbtest.NewEventsFetcher(t, getConfig())
 	err := mbtest.WriteEvents(f, t)
 	if err != nil {
 		t.Fatal("write", err)
 	}
+=======
+func TestFetch(t *testing.T) {
+	f := mbtest.NewEventsFetcher(t, getConfig())
+	event, err := f.Fetch()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(" module : %s metricset : %s event: %+v", f.Module().Name(), f.Name(), event)
+>>>>>>> add integration tests to network metricset
 }
 
 func getConfig() map[string]interface{} {
@@ -21,6 +34,9 @@ func getConfig() map[string]interface{} {
 		"module":     "docker",
 		"metricsets": []string{"network"},
 		"hosts":      []string{"localhost"},
+<<<<<<< b15c311f2fa9a3beb220fcf17cddcff370c7abcc
 		"socket":     "unix:///var/run/docker.sock",
+=======
+>>>>>>> add integration tests to network metricset
 	}
 }
