ce990d92b1a5bee6916304c641591674f3bf39c2
Douae Jeouit <douae.jeouit@gmail.com>
2016-09-29 16:39:53 +0200

    add integration tests to diskio metricset

--- metricbeat/module/docker/diskio/diskio_integration_test.go
+++ metricbeat/module/docker/diskio/diskio_integration_test.go
@@ -1,5 +1,8 @@
+<<<<<<< 2945c1437b846caac981cc9d67f0b8e40da6c938
 // +build integration
 
+=======
+>>>>>>> add integration tests to diskio metricset
 package diskio
 
 import (
@@ -8,12 +11,22 @@ import (
 	mbtest "github.com/elastic/beats/metricbeat/mb/testing"
 )
 
+<<<<<<< 2945c1437b846caac981cc9d67f0b8e40da6c938
 func TestData(t *testing.T) {
 	f := mbtest.NewEventsFetcher(t, getConfig())
 	err := mbtest.WriteEvents(f, t)
 	if err != nil {
 		t.Fatal("write", err)
 	}
+=======
+func TestFetch(t *testing.T) {
+	f := mbtest.NewEventsFetcher(t, getConfig())
+	event, err := f.Fetch()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(" module : %s metricset : %s event: %+v", f.Module().Name(), f.Name(), event)
+>>>>>>> add integration tests to diskio metricset
 }
 
 func getConfig() map[string]interface{} {
@@ -21,6 +34,9 @@ func getConfig() map[string]interface{} {
 		"module":     "docker",
 		"metricsets": []string{"diskio"},
 		"hosts":      []string{"localhost"},
+<<<<<<< 2945c1437b846caac981cc9d67f0b8e40da6c938
 		"socket":     "unix:///var/run/docker.sock",
+=======
+>>>>>>> add integration tests to diskio metricset
 	}
 }
