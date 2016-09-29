93aa34be80766d7f4bf99d88834c5d17a52481b5
Douae Jeouit <douae.jeouit@gmail.com>
2016-09-29 18:36:48 +0200

    Add  unit tests to network metricset :  case : interface x for container y first created

--- metricbeat/module/docker/network/network_test.go
+++ metricbeat/module/docker/network/network_test.go
@@ -1,16 +1,149 @@
 package network
 
 import (
+	"github.com/elastic/beats/libbeat/common"
+	dc "github.com/fsouza/go-dockerclient"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 	"time"
+
+	"github.com/elastic/beats/metricbeat/module/docker"
 )
 
 var netService NETService
 var oldNetRaw NETRaw
 var newNetRaw NETRaw
 
-func TestGetRxBytesPerSecond(t *testing.T) {
+// Case : old interface  x of the container y !exist
+func TestGetNetworkStatsFirstEvent(t *testing.T) {
+
+	//GIVEN
+	containerID := "containerID"
+	labels := map[string]string{
+		"label1": "val1",
+		"label2": "val2",
+	}
+	container := dc.APIContainers{
+		ID:         containerID,
+		Image:      "image",
+		Command:    "command",
+		Created:    123789,
+		Status:     "Up",
+		Ports:      []dc.APIPort{{PrivatePort: 1234, PublicPort: 4567, Type: "portType", IP: "123.456.879.1"}},
+		SizeRw:     123,
+		SizeRootFs: 456,
+		Names:      []string{"/container1"},
+		Labels:     labels,
+		Networks:   dc.NetworkList{},
+	}
+	networks := make(map[string]dc.NetworkStats, 2)
+	networks["eth0"] = dc.NetworkStats{
+		RxBytes:   100,
+		RxDropped: 200,
+		RxErrors:  300,
+		RxPackets: 400,
+		TxBytes:   500,
+		TxDropped: 600,
+		TxErrors:  700,
+		TxPackets: 800,
+	}
+	networks["eth1"] = dc.NetworkStats{
+		RxBytes:   900,
+		RxDropped: 1000,
+		RxErrors:  1100,
+		RxPackets: 1200,
+		TxBytes:   1300,
+		TxDropped: 1400,
+		TxErrors:  1500,
+		TxPackets: 1600,
+	}
+	// create network stats
+	tmp := time.Now()
+	mystats := dc.Stats{}
+	mystats.Networks = make(map[string]dc.NetworkStats)
+	mystats.Networks = networks
+	mystats.Read = tmp
+	//create dockerStats
+	networkStatsStruct := []docker.DockerStat{}
+	networkStatsStruct = append(networkStatsStruct, docker.DockerStat{
+		Container: container,
+		Stats:     mystats,
+	})
+	//expected events
+	expectedEvents := []common.MapStr{}
+	expectedEvents = append(expectedEvents, common.MapStr{
+		"@timestamp": tmp,
+		"container": common.MapStr{
+			"id":     containerID,
+			"name":   "container1",
+			"labels": docker.BuildLabelArray(labels),
+		},
+		"socket": docker.GetSocket(),
+		"eth0": common.MapStr{
+			"rx": common.MapStr{
+				"bytes":   0,
+				"dropped": 0,
+				"errors":  0,
+				"packets": 0,
+			},
+			"tx": common.MapStr{
+				"bytes":   0,
+				"dropped": 0,
+				"errors":  0,
+				"packets": 0,
+			},
+		}})
+	expectedEvents = append(expectedEvents, common.MapStr{
+		"@timestamp": tmp,
+		"container": common.MapStr{
+			"id":     containerID,
+			"name":   "container1",
+			"labels": docker.BuildLabelArray(labels),
+		},
+		"socket": docker.GetSocket(),
+		"eth1": common.MapStr{
+			"rx": common.MapStr{
+				"bytes":   0,
+				"dropped": 0,
+				"errors":  0,
+				"packets": 0,
+			},
+			"tx": common.MapStr{
+				"bytes":   0,
+				"dropped": 0,
+				"errors":  0,
+				"packets": 0,
+			},
+		},
+	})
+
+	networkService := NETService{
+		NetworkStatPerContainer: make(map[string]map[string]NETRaw),
+	}
+	networkService.NetworkStatPerContainer[containerID] = make(map[string]NETRaw)
+	//networkService.NetworkStatPerContainer[containerID]["eth0"]= NETRaw{}
+	event := networkService.GetNetworkStatsPerContainer(networkStatsStruct)
+	formattedStats := eventsMapping(event)
+	assert.True(t, equalEvent(expectedEvents, formattedStats))
+	//t.Logf("expected events : %v", expectedEvents)
+	//t.Logf("formated events : %v", formattedStats)
+}
+func getNetworkCalculatorMocked(number float64) MockNetworkCalculator {
+	mockedNetworkCalculator := MockNetworkCalculator{}
+	mockedNetworkCalculator.On("getRxBytesPerSecond").Return(number)
+	mockedNetworkCalculator.On("getRxDroppedPerSecond").Return(number * 2)
+	mockedNetworkCalculator.On("getRxErrorsPerSecond").Return(number * 3)
+	mockedNetworkCalculator.On("getRxPacketsPerSecond").Return(number * 4)
+	mockedNetworkCalculator.On("getTxBytesPerSecond").Return(number * 5)
+	mockedNetworkCalculator.On("getTxDroppedPerSecond").Return(number * 6)
+	mockedNetworkCalculator.On("getTxErrorsPerSecond").Return(number * 7)
+	mockedNetworkCalculator.On("getTxPacketsPerSecond").Return(number * 8)
+	return mockedNetworkCalculator
+
+}
+
+/*func TestGetRxBytesPerSecond(t *testing.T) {
 	setTime()
 	// set old & new rxBytes
 	oldNetRaw.RxBytes = 20
@@ -80,5 +213,10 @@ func TestGetTxErrorsPerSecond(t *testing.T) {
 
 func setTime() {
 	oldNetRaw.Time = time.Now()
-	newNetRaw.Time = oldNetRaw.Time.Add(time.Duration(2000000000))
+	newNetRaw.Time = time.Now()//oldNetRaw.Time.Add(time.Duration(2000000000))
+}*/
+func equalEvent(expectedEvent []common.MapStr, event []common.MapStr) bool {
+
+	return reflect.DeepEqual(expectedEvent, event)
+
 }
